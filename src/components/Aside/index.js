19fe3e4bff7f148b2baf56ea40ed1b0c4b6b458d
Iano Maciel <ianomaciel6385@gmail.com>
2023-02-07 14:39:47 -0400

    feat: side menu creation

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import * as S from './styles';
 
+import IconSolicita from '../../assets/solicitaMenu.svg'
+
+
+import { 
+    MdOutlinePermIdentity,
+    MdPlaylistAdd,
+    MdOutlineRule
+ } from "react-icons/md";
+
 
 
 const Aside = () => {
     return(
         <S.Container>
-            <h1>Aside</h1>
+            <S.MenuContainer>
+                <h3>Perfil</h3>
+                <S.MenuItemLink href="#"> <MdOutlinePermIdentity/> Meus dados</S.MenuItemLink>
+                <h3>Aproveitamento</h3>
+                <S.MenuItemLink href="#"><MdPlaylistAdd/>Solicitação</S.MenuItemLink>
+                <S.MenuItemLink href="#"><MdOutlineRule/>Status de solicitação</S.MenuItemLink>
+            </S.MenuContainer>
         </S.Container>
     );
 }
--- src/components/Aside/styles.js
+++ src/components/Aside/styles.js
@@ -2,5 +2,48 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     grid-area: AS;
-    background-color: #D9D9D9;
+    background-color: #EBEBEB;
+
+    padding-left: 20px;
+`;
+
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+
+    h3 {
+        font-size: 24px;
+        color: #000;
+    }
+`;
+
+export const MenuItemLink = styled.a`
+    display: flex;
+    align-items: center;
+
+    color: #fff;
+    font-size: 20px;
+    text-decoration: none;
+
+    
+    background-color: #137741;
+    padding: 10px;
+    border-radius: 10px;
+    width: 95%;
+
+
+    margin: 10px 0;
+
+    transition: opacity .3s;
+
+    &:hover {
+        opacity: .5;
+    }
+
+    > svg {
+        margin-right: 15px;
+        font-size: 35px;
+        color: #fff;
+    }
 `;
