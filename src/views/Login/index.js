fc5c88b27e7afd30754a7a6ed987454d5d84e419
Iano Maciel <ianomaciel6385@gmail.com>
2023-02-08 16:14:05 -0400

    feat: login screen development

---
+++ src/views/Login/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import * as S from './styles'
+import fundo from '../../assets/logo.svg'
+
+const Login = () => {
+    return(
+        <S.Container>
+            <S.Box>
+                <h2>LOGIN</h2>
+                <span>Tá a fim de solicitar suas horas complementares, né?</span>
+                <S.Form>
+                    <input type="text" placeholder="Matrícula"/>
+                    <input type="password" placeholder="Senha"/>
+
+                    <button type="button">Entrar</button>
+                </S.Form>
+
+                <S.NewAccess>
+                    Seu primeiro acesso?
+                    <a href="/cadAluno"> Cadastre aqui!</a>
+                </S.NewAccess>
+            </S.Box>
+        </S.Container>
+    );
+}
+
+export default Login;
